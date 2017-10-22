9516764a4d7424c4c8443b7eeb08995e274939f2
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2017-10-22 15:51:11 -0500

    use object destructure for `A` class

--- src/components/a.tsx
+++ src/components/a.tsx
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { trackOutboundLink } from "../utils/gtag";
 
-export class A extends React.PureComponent<{
-  href: string;
-  className?: string;
-}, {}> {
+export class A extends React.PureComponent<React.AnchorHTMLAttributes<HTMLAnchorElement>, {}> {
   render() {
     const { href } = this.props;
     // @todo use regex ^http(s)://
     const target = href.startsWith("http") ? "_blank" : "_self";
     return <a
-      href={href}
       target={target}
-      className={this.props.className}
+      {... this.props}
       onClick={(e)=>{
         // @todo e.preventDefault(); is here, but document.location is inside trackOutboundLink is dirty code
         e.preventDefault();
